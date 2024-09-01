d2c120f0f292889f6ca23b10ac4272b969ab35d1
BENICHAI MIKHAEL 140 <benichaimikhael@gmail.com>
2024-09-02 01:09:38 +0300

    Add files via upload

---
+++ hw 1.py
@@ -0,0 +1,26 @@
+# start
+
+x: int = int (input ('where is x?'))
+
+
+y: int = int (input ('where is y?'));
+
+z: int = int (input ('where is z?'));
+
+result: int = x + y + z;
+
+multi: int =  x * y * z;
+
+print (result)
+
+print (multi)
+
+
+
+
+
+
+
+
+
+# end
